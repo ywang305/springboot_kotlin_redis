805c26ca1b3b68a47a1669b7d29f32a6002bfecf
yao <ywang305@syr.edu>
2019-09-14 19:05:53 -0400

    gradle redis setup and integrated

---
+++ src/main/kotlin/io/rest/api/controller/RedisController.kt
@@ -0,0 +1,32 @@
+package io.rest.api.controller
+
+import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+import java.util.concurrent.atomic.AtomicLong
+
+@RestController
+@RequestMapping(path = ["/redis"], produces = [MediaType.APPLICATION_JSON_UTF8_VALUE])
+class RedisController(private val redisTemplate: RedisTemplate<Any, Any>) {
+    val atom = AtomicLong()
+
+    @GetMapping
+    fun get(id: Long?): Any? {
+        val isExist = redisTemplate.opsForHash<String,JobVar>().hasKey(id, "jobvar")
+        return if(isExist)
+            redisTemplate.opsForHash<Any,Any>().get(id, "jobvar")
+            else null
+    }
+
+    @PostMapping
+    fun post(@RequestBody jobVar: JobVar): ResponseEntity<Any> {
+        val uniqueId = atom.incrementAndGet()
+        redisTemplate.opsForHash<String,JobVar>().put(uniqueId, "jobvar", jobVar)
+        return ResponseEntity.ok().body(object{ val id = uniqueId})
+    }
+}
+
+data class JobVar(val date: String,
+                  val time: String,
+                  val addressList: List<String>)
